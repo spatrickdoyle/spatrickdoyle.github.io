e52d965dc9a47eec09fd669ae84a7340c499eb13
thebonaestest <thebonaestest@gmail.com>
2016-06-08 14:12:10 -0500

    optimized a little more for mobile

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var modal = document.getElementById('modal-bg');
 var btn = document.getElementById("contact_button");
 
+var vw = document.documentElement.clientWidth;
+
 btn.onmouseover = function() {
+	vw = document.documentElement.clientWidth;
+
+	if (vw > 980) {
+		modal.style.display = "block";
+	}
+}
+
+btn.onclick = function() {
 	modal.style.display = "block";
 }
 
