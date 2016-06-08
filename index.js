388d510b988255c94a100061260ae334dcccf1f1
thebonaestest <thebonaestest@gmail.com>
2016-06-08 13:40:01 -0500

    fixed some formatting stuff, made the contact button scrollover, cleared some extra crap out

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var modal = document.getElementById('modal-bg');
 var btn = document.getElementById("contact_button");
 
-btn.onclick = function() {
+btn.onmouseover = function() {
 	modal.style.display = "block";
 }
 
-window.onclick = function(event) {
+window.onmouseover = function(event) {
 	if (event.target == modal) {
 		modal.style.display = "none";
 	}
