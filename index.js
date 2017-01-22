6b47489d22766cab0a96e362cfd0a78cd152da24
spatrickdoyle <sean@spatrickdoyle.com>
2017-01-22 13:24:07 -0600

    added cool animation to the home page

--- index.js
+++ index.js
@@ -201,3 +201,87 @@ window.onscroll = function() {
 		document.getElementById("bar").setAttribute("style", "position: fixed");
 	}
 }
+
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+	this.urls = [ "", "rice.html", "vision.html", "website.html", "knw.html", "vision.html", "spectre.html", "vision.html", "me.html" ];
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+
+	this.cursor = document.getElementById("cursor");
+	this.cursor2 = document.getElementById("cursor2");
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+	var url = this.urls[i];
+	console.log(fullTxt,url);
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    }
+	else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+
+		if (this.cursor) {
+			this.cursor.style.animationPlayState = "running";
+			this.cursor.style.display = "inline-block";
+			this.cursor2.style.display = "none";
+		}
+
+		this.el.href = url;
+    }
+	else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+
+		if (this.cursor) {
+			this.cursor.style.animationPlayState = "running";
+			this.cursor.style.display = "inline-block";
+			this.cursor2.style.display = "none";
+		}
+    }
+	else {
+		if (this.cursor) {
+			this.cursor.style.animationPlayState = "paused";
+			this.cursor.style.display = "none";
+			this.cursor2.style.display = "inline-block";
+		}
+
+		this.el.href = "";
+	}
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type')
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+};
